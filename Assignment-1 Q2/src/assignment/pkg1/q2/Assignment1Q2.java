62e140a03c35bb0db9c6da30982cac35d4ae86e4
Prankur Agarwal <prankur.agarwal18@gmail.com>
2016-09-15 23:16:35 +0530

    Add files via upload

---
+++ Assignment-1 Q2/src/assignment/pkg1/q2/Assignment1Q2.java
@@ -0,0 +1,25 @@
+package assignment.pkg1.q2;
+import java.util.*;
+
+//prankuragarwal
+
+public class Assignment1Q2 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print ( "Enter no. of sheets of paper: " );
+        int sheets = s.nextInt();
+        
+        int stamps;
+        
+        stamps = sheets/5;
+        
+        if ( sheets%5 !=0 ) {
+            stamps++;
+        }
+        
+        System.out.println ( " No. of stamps required is " + stamps );
+ 
+    }
+    
+}
